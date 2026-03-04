4336cccf32d02a40dd9be0ffa8ddd11d6331c75a
Raul Martinez Beltran <ramartin@ucsd.edu>
2026-03-04 11:51:51 -0800

    Added changes for config to build and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://raul-martinezbeltran.github.io/Personal-Website/#",
+  base: "Personal-Website/",
   plugins: [react(), tailwindcss()],
 });
