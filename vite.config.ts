b96e960b1f5772b0135fbef2bcb4503b12ba613a
Raul Martinez Beltran <ramartin@ucsd.edu>
2026-03-04 11:46:47 -0800

    Added pound sign for hash router

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://raul-martinezbeltran.github.io/Personal-Website/",
+  base: "https://raul-martinezbeltran.github.io/Personal-Website/#",
   plugins: [react(), tailwindcss()],
 });
